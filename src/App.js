a79d95ce7d7cbb578abeea2aa9e6a4473a19c98e
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-06-09 21:15:10 +0100

    Exercises 1, 2, and 3 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import Headers from "./Headers";
 import Bookings from "./Bookings";
+import TouristInfoCards from "./TouristInfoCards";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Headers />
       <Bookings />
+      <TouristInfoCards />
     </div>
   );
 };
