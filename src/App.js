f3504e2480af3046ae2c50458a0d7388a4578d33
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-06-18 15:36:35 +0100

    Added Tourist Cards and created new css file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Heading from "./Heading";
 import Bookings from "./Bookings";
 import "./App.css";
 import "./Heading.css";
+import "./TouristInfoCards.css";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
