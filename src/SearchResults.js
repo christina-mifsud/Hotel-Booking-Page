e7750f9edfe8257bec405cb613e650b73b3a46e7
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-06-24 10:54:05 +0100

    L1-5 Created a table
    Imported SearchResults and fake bookings

---
+++ src/SearchResults.js
@@ -0,0 +1,34 @@
+import react from "react";
+
+const SearchResults = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>First Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+          <th>Room ID</th>
+          <th>Check In Date</th>
+          <th>Check Out Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>A</td>
+          <td>B</td>
+          <td>C</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default SearchResults;
