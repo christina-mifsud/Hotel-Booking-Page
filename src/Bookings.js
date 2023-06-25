e8cc369551bbeed2d6423661318ee8b2f597e135
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-06-25 15:50:10 +0100

    L2-14 Created new useState for bookings

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
@@ -8,11 +8,16 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
+  // initialise bookings state with FakeBookings
+  // i.e. when the page first loads up data in FakeBookings will show
+  // setBookings function will be called later to change the state with filter
+  const [bookings, setBookings] = useState(FakeBookings);
+
   return (
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={FakeBookings} />
+        <SearchResults results={bookings} />
       </div>
     </div>
   );
